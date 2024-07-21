81c84614a4b3b5bc790a66a3a69b8d1073279a73
taiyo <nishimototai@gmail.com>
2024-07-21 12:01:54 +0900

    tessoku a 32, this is true

---
+++ tessoku/A_32/Main.java
@@ -0,0 +1,36 @@
+package tessoku.A_32;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+
+
+        boolean[] dp = new boolean[n + 2];
+
+        for (int i = 0; i <= n; i++) {
+            if (i > a && dp[i-a] == false) {
+                dp[i] = true;
+            } else if (i >= b && dp[i-b] == false) {
+                dp[i] = true;
+            } else {
+                dp[i] = false;
+            }
+            
+        }
+
+        if (dp[n] == true) {
+            System.out.println("First");
+        } else {
+            System.out.println("Second");
+        }
+
+        sc.close();
+    }
+}
